ed7bf73dc07e108cfa01aa03ff4c0a9a597ed8b1
Sidragon123 <siddhanth.subramanian99@gmail.com>
2024-07-24 17:58:07 +0800

    FE-feature-add-checklist-page table commit 1

--- views/src/App.jsx
+++ views/src/App.jsx
@@ -4,6 +4,7 @@ import Signup from "./Pages/Signup";
 import Interndashboard from "./Pages/Interndashboard";
 import Mentordashboard from "./Pages/Mentordashboard";
 import SingleFeedBack from "./Pages/SingleFeedbackPage";
+import Checklist from "./Pages/Checklist";
 import Homepage from "./Pages/HomePage";
 import Unauthorized from "./Pages/Unauthorized";
 import Notfound from "./Pages/Notfound";
@@ -14,18 +15,45 @@ import AuthWrapper from "./Utility/AuthWrapper";
 function App() {
   return (
     <MantineProvider>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/login" element={<Loginpage />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/intern/*" element={<AuthWrapper>{({ user }) => <Interndashboard user={user} />}</AuthWrapper>} />
-          <Route path="/mentor/*" element={<AuthWrapper>{({ user }) => <Mentordashboard user={user} />}</AuthWrapper>} />
-          <Route path="/feedback/:id" element={<AuthWrapper>{({ user }) => <SingleFeedBack user={user} />}</AuthWrapper>} />
-          <Route path="/403" element={<Unauthorized />} />
-          {/* wasnt sure how to do this correctly but this route enables pages that dont match our routes to land
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Loginpage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/intern/*"
+          element={
+            <AuthWrapper>
+              {({ user }) => <Interndashboard user={user} />}
+            </AuthWrapper>
+          }
+        />
+        <Route
+          path="/mentor/*"
+          element={
+            <AuthWrapper>
+              {({ user }) => <Mentordashboard user={user} />}
+            </AuthWrapper>
+          }
+        />
+        <Route
+          path="/feedback/:id"
+          element={
+            <AuthWrapper>
+              {({ user }) => <SingleFeedBack user={user} />}
+            </AuthWrapper>
+          }
+        />
+        <Route
+          path="/checklist/*"
+          element={
+            <AuthWrapper>{({ user }) => <Checklist user={user} />}</AuthWrapper>
+          }
+        />
+        <Route path="/403" element={<Unauthorized />} />
+        {/* wasnt sure how to do this correctly but this route enables pages that dont match our routes to land
           on the 404 page */}
-          <Route path="*" element={<Notfound />} />
-        </Routes>
+        <Route path="*" element={<Notfound />} />
+      </Routes>
     </MantineProvider>
   );
 }
