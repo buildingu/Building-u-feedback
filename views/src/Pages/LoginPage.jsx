040e61a65324b5e49968ffe2d257d6ed2bcc89be
Shruti Verma <shrtiverma032003@gmail.com>
2024-08-18 19:59:04 +0530

    make changes on login page to improve UX

--- views/src/Pages/LoginPage.jsx
+++ views/src/Pages/LoginPage.jsx
@@ -140,7 +140,7 @@ function LoginPage() {
           <div style={{ marginTop: "1.5rem" }}>
             <Group position="center" align="center" spacing="xs">
               <Text size="lg" c="dimmed">
-                Don't have an account?
+                Don't have an account ?
               </Text>
               <Anchor
                 onClick={() => navigate("/signup")}
